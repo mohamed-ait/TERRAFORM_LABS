da2858b3fc499de2e879dc1d35795f78cfd3a8b5
moaitlahcen <mohamedaitlahcen62@gamil.com>
2024-09-21 16:38:15 +0100

    Add block to create aws_key_pair

--- aws_ec2/main.tf
+++ aws_ec2/main.tf
@@ -20,3 +20,8 @@ resource "tls_private_key" "rsa_4096" {
   rsa_bits  = 4096
 }
 
+// Create Key Pair for Connecting EC2 via SSH
+resource "aws_key_pair" "key_pair" {
+  key_name   = var.key_name
+  public_key = tls_private_key.rsa_4096.public_key_openssh
+}
