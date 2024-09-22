f7b381be2e10a1d5c9202f27f654532c498c4301
moaitlahcen <mohamedaitlahcen62@gamil.com>
2024-09-22 21:50:27 +0100

    Create variables

---
+++ azure_keyVault/variables.tf
@@ -0,0 +1,15 @@
+variable rg_name {
+  type        = string
+  default     = "rg"
+  description = "resource group name"
+}
+
+variable location {
+  type        = string
+  default     = "eu-west-1"
+  description = "region"
+}
+
+#############################
+# Azure Key Vault variables #
+#############################
