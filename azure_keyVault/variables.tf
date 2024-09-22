b41f49487703be67fada92f5c43fc342aa40904e
moaitlahcen <mohamedaitlahcen62@gamil.com>
2024-09-22 22:01:40 +0100

    Add policies variables to manage policies

--- azure_keyVault/variables.tf
+++ azure_keyVault/variables.tf
@@ -27,3 +27,16 @@ variable "secrets" {
   description = "Define Azure Key Vault secrets"
   default = {}
 }
+
+variable "policies" {
+  type = map(object({
+    tenant_id               = string
+    object_id               = string
+    key_permissions         = list(string)
+    secret_permissions      = list(string)
+    certificate_permissions = list(string)
+    storage_permissions     = list(string)
+  }))
+  description = "Define a Azure Key Vault access policy"
+  default = {}
+}
