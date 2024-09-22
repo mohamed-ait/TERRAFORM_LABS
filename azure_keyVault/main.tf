5e240e8c51832815daf9d75768984699a97f0ba0
moaitlahcen <mohamedaitlahcen62@gamil.com>
2024-09-22 23:40:42 +0100

    Updated main.tf

--- azure_keyVault/main.tf
+++ azure_keyVault/main.tf
@@ -52,3 +52,30 @@ resource "azurerm_key_vault_access_policy" "policy" {
   secret_permissions       = ["Get", "List", "Set", "Delete"]
   
 }
+
+# Generate a random password
+resource "random_password" "password" {
+  for_each    = var.secrets
+  length      = 5
+  min_upper   = 2
+  min_lower   = 2
+  min_numeric = 2
+  min_special = 2
+
+  keepers = {
+    name = each.key
+  }
+}
+
+# Create an Azure Key Vault secrets
+resource "azurerm_key_vault_secret" "secret" {
+  for_each     = var.secrets
+  key_vault_id = azurerm_key_vault.azureKeyVault.id
+  name         = each.key
+  value        = lookup(each.value, "value") != "" ? lookup(each.value, "value") : random_password.password[each.key].result
+  tags         = var.tags
+  depends_on = [
+    azurerm_key_vault.key-vault,
+    azurerm_key_vault_access_policy.default_policy,
+  ]
+}
