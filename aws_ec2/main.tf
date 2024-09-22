0a87ecace677f4eb9ba5669cde818e0b7576a3ef
moaitlahcen <mohamedaitlahcen62@gamil.com>
2024-09-22 16:52:56 +0100

    Updated main.tf

--- aws_ec2/main.tf
+++ aws_ec2/main.tf
@@ -51,3 +51,19 @@ resource "aws_security_group" "sg_ec2" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+
+resource "aws_instance" "public_instance" {
+  ami                    = "ami-0f5ee92e2d63afc18"
+  instance_type          = "t2.micro"
+  key_name               = aws_key_pair.key_pair.key_name
+  vpc_security_group_ids = [aws_security_group.sg_ec2.id]
+
+  tags = {
+    Name = "public_instance"
+  }
+
+  root_block_device {
+    volume_size = 30
+    volume_type = "gp2"
+  }
+}
