e5d42ad919c33d4f2bc58ccccbb3f2411906a1ab
moaitlahcen <mohamedaitlahcen62@gamil.com>
2024-09-22 22:05:26 +0100

    Add azurerm_key_vault_access_policy resource to manage keyVault pilicies

--- azure_keyVault/main.tf
+++ azure_keyVault/main.tf
@@ -29,3 +29,15 @@ resource "azurerm_key_vault" "azureKeyVault" {
   sku_name = "standard"
 
 }
+
+# Create an Azure Key Vault access policy
+resource "azurerm_key_vault_access_policy" "policy" {
+  for_each                = var.policies
+  key_vault_id            = azurerm_key_vault.key-vault.id
+  tenant_id               = lookup(each.value, "tenant_id")
+  object_id               = lookup(each.value, "object_id")
+  key_permissions         = lookup(each.value, "key_permissions")
+  secret_permissions      = lookup(each.value, "secret_permissions")
+  certificate_permissions = lookup(each.value, "certificate_permissions")
+  storage_permissions     = lookup(each.value, "storage_permissions")
+}
